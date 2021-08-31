d022b7e9435d44b296137292c7f2f7072ab40529
KennyyonCoding <89782353+KennyyonCoding@users.noreply.github.com>
2021-08-31 10:56:22 +1000

    2nd Commit

--- candicegame.py
+++ candicegame.py
@@ -5,7 +5,7 @@ input(f"{gaming.title()}?! What an odd name?\nDo you like your name? ")
 gama = input(f"Wait? {gaming} is not your name?!\nTell me your real name then: ")
 troll1 = input(f"{gama}, huh, interesting name.\nAny way do you know who Candice is?: ")
 if troll1.lower() == 'who':
-    print('Candice balls fit in yo mouth. Lmaoooo')
+    print('Candice ba   lls fit in yo mouth. Lmaoooo')
     exit()
 troll2 = input(f"{troll1}?? That is not the answer.\nIt seems like you clearly don't know. Ask me who: ")
 if troll2.lower() == 'who':
